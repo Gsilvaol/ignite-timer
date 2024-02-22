132e266e6856eda3edf7859dfe708e9f09259230
Gustavo Silva de Oliveira <gustavosilva1994_@hotmail.com>
2024-02-22 15:56:58 -0300

    aprimorando os inputs

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -50,6 +50,10 @@ const BaseInput = styled.input`
 
 export const TaskInput = styled(BaseInput)`
   flex: 1;
+
+  &::-webkit-calendar-picker-indicator {
+    display: none !important;
+  }
 `
 
 export const MinutesAmountInput = styled(BaseInput)`
@@ -86,6 +90,10 @@ export const StartCountdownButton = styled.button`
   border: 0;
   padding: 1rem;
   border-radius: 8px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
   gap: 0.5rem;
   font-weight: bold;
