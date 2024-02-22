dc6fc52d40853330935ee52ece3eb6a0512abfd5
Gustavo Silva de Oliveira <gustavosilva1994_@hotmail.com>
2024-02-22 12:07:26 -0300

    header e layout

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { History } from './pages/History'
-import { DefaultLayout } from './layouts/Default.layout'
+import { DefaultLayout } from './layouts/DefaultLayoult'
 
 export function Router() {
   return (
