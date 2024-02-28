fda7323056276a5c61e9241a40afdab0b1856846
Gustavo Silva de Oliveira <gustavosilva1994_@hotmail.com>
2024-02-27 21:33:51 -0300

    typescript no formulario

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -18,12 +18,21 @@ const newCycleFormValidationSchema = zod.object({
   MinutesAmount: zod.number().min(5).max(60),
 })
 
+interface NewCycleFormData {
+  task: string
+  minutesAmount: number
+}
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm({
+  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
   })
 
-  function handleCreateNewCycle(data: any) {
+  function handleCreateNewCycle(data: NewCycleFormData) {
     console.log(data)
   }
 
